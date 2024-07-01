05c87ae795fd692390817f1d5237e3160aa55592
sefasezr <153550790+sefasezr@users.noreply.github.com>
2024-07-01 23:42:42 +0300

    Appointment olusturuldu

--- src/AppointmentApp/Appointment.java
+++ src/AppointmentApp/Appointment.java
@@ -1,4 +1,19 @@
 package AppointmentApp;
-
+//6-Randevu:
+//              -id: otomatik üretilsin
+//              -hasta adi,doktor,randevu tarihi
 public class Appointment {
+    private static int counter = 0;
+    private int id;
+    private String name;
+    private Doctor doctor;
+    private String date;
+
+    public Appointment(String name, Doctor doctor, String date) {
+        counter++;
+        this.id = counter;
+        this.name = name;
+        this.doctor = doctor;
+        this.date = date;
+    }
 }
